57af5f1a874ba455bcbfeb48f8e9a61862de0be9
Andy Oliver Rozario <andyrozario7@gmail.com>
2025-08-05 13:19:44 +0530

    Fix layout.tsx client/server component issue by creating ClientLayoutWrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { MysticalFeedback } from "@/components/MysticalFeedback"
-import { Footer } from "@/components/Footer"
-import { Toaster } from "@/components/ui/toaster"
-import { AnalyticsInitializer } from "@/components/AnalyticsInitializer"
-import { HamburgerMenu } from "@/components/HamburgerMenu"
-import { AutoInitializer } from "@/components/AutoInitializer"
 import ClientProviders from "@/components/ClientProviders"
+import { ClientLayoutWrapper } from "@/components/ClientLayoutWrapper"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -66,13 +61,8 @@ export default function RootLayout({
           <div className="absolute inset-0 bg-gradient-to-b from-slate-950/20 via-transparent to-slate-950/40" />
           <div className="relative z-10">
             <ClientProviders>
-              <AutoInitializer />
-              <AnalyticsInitializer />
               {children}
-              <HamburgerMenu />
-              <MysticalFeedback />
-              <Footer />
-              <Toaster />
+              <ClientLayoutWrapper />
             </ClientProviders>
           </div>
         </div>
